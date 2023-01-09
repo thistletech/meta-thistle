8bc972d52c312b767415ba6e1eaa2be31f22cdee
Scott Hollwedel <scott@thistle.tech>
2023-01-09 14:14:43 -0800

    Fix bootargs for bootcmds

--- meta-secure-platform-bsp/recipes-bsp/u-boot/u-boot-base-scr/raspberrypi4/boot.cmd
+++ meta-secure-platform-bsp/recipes-bsp/u-boot/u-boot-base-scr/raspberrypi4/boot.cmd
@@ -4,8 +4,8 @@ fdt addr ${fdt_addr} && fdt get value base_bootargs /chosen bootargs
 setenv fit_addr 0x1F000000
 
 # Set A/B boot commands
-setenv boot_cmd_a "setenv bootargs ${base_bootargs} root=/dev/mmcblk0p2 rootfstype=ext4 rootwait; ext4load mmc 0:2 ${fit_addr} /boot/fitImage; bootm ${fit_addr} - ${fdt_addr}"
-setenv boot_cmd_b "setenv bootargs ${base_bootargs} root=/dev/mmcblk0p3 rootfstype=ext4 rootwait; ext4load mmc 0:3 ${fit_addr} /boot/fitImage; bootm ${fit_addr} - ${fdt_addr}"
+setenv boot_cmd_a "setenv bootargs '${base_bootargs} root=/dev/mmcblk0p2'; ext4load mmc 0:2 ${fit_addr} /boot/fitImage; bootm ${fit_addr} - ${fdt_addr}"
+setenv boot_cmd_b "setenv bootargs '${base_bootargs} root=/dev/mmcblk0p3'; ext4load mmc 0:3 ${fit_addr} /boot/fitImage; bootm ${fit_addr} - ${fdt_addr}"
 
 # Command to source the script to manage dualboot
 setexpr custom_scriptaddr ${scriptaddr} + 0x4000
